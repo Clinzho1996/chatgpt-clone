bba4f816edc4a9475e81acd2f053c62b7b3a345c
Confidence Emonena <confidinho@yahoo.com>
2023-11-01 11:05:05 +0100

    firestore update

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -4,7 +4,7 @@ import { useRouter, usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { query, collection, orderBy } from "firebase/firestore";
+import { query, collection, orderBy, deleteDoc, doc } from "firebase/firestore";
 import { db } from "@/firebase";
 
 type Props = {
@@ -29,6 +29,11 @@ function ChatRow({ id }: Props) {
     setActive(pathname.includes(id));
   }, [pathname]);
 
+  const removeChat = async () => {
+    await deleteDoc(doc(db, "users", session?.user?.email!, "chats", id));
+    router.replace("/");
+  };
+
   return (
     <Link
       href={`/chat/${id}`}
@@ -38,7 +43,10 @@ function ChatRow({ id }: Props) {
       <p className="flex-1 hidden md:inline-flex truncate">
         {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
       </p>
-      <TrashIcon className="h-5 w-5 text-gray-700 hover:text-red-700" />
+      <TrashIcon
+        onClick={removeChat}
+        className="h-5 w-5 text-gray-700 hover:text-red-700"
+      />
     </Link>
   );
 }
