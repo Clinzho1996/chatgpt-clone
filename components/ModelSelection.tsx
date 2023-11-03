5153ebcb14445673b47503c2d67bc2e1df569504
Confidence Emonena <confidinho@yahoo.com>
2023-11-03 04:58:31 +0100

    model selection

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import useSWR from "swr";
+import Select from "react-select";
+
+const fetchModels = () => fetch("/api/getEngines").then((res) => res.json());
 
 function ModelSelection() {
-  return <div>ModelSelection</div>;
+  const { data: models, isLoading } = useSWR("models", fetchModels);
+  const { data: model, mutate: setModel } = useSWR("model", {
+    fallbackData: "text-davinci-003",
+  });
+
+  // Custom styles for react-select
+  const customStyles = {
+    control: (provided: any) => ({
+      ...provided,
+      backgroundColor: "#434654", // Set the background color for the control
+      border: `1px solid #434654`,
+      color: "#fff", // Set the border color
+    }),
+  };
+
+  return (
+    <div>
+      <Select
+        className="mt-2"
+        options={models?.modelOptions}
+        defaultValue={model}
+        placeholder={model}
+        isSearchable
+        isLoading={isLoading}
+        menuPosition="fixed"
+        styles={customStyles} // Apply custom styles
+        onChange={(e) => setModel(e.value)}
+      />
+    </div>
+  );
 }
 
 export default ModelSelection;
