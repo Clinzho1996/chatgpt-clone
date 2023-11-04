a159b904e43c7389c585d5d37469f8b660cc289b
Confidence Emonena <confidinho@yahoo.com>
2023-11-04 06:52:14 +0100

    done and dusted

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,19 +1,15 @@
 "use client";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
+import Bot from "../public/bot.png";
 
 function Login() {
   return (
-    <div className="bg-[#11a37f] h-screen flex flex-col items-center justify-center text-center">
-      <Image
-        src="https://links.papareact.com/2i6"
-        width={300}
-        height={300}
-        alt="logo"
-      />
+    <div className="bg-[#00082F] h-screen flex flex-col items-center justify-center text-center">
+      <Image src={Bot} width={200} height={200} alt="logo" />
       <button
         onClick={() => signIn("google")}
-        className="text-white font-bold text-3xl animate-pulse"
+        className="text-[#00082F] p-3 mt-4 rounded-md  bg-white font-bold text-2xl animate-pulse"
       >
         Sign in to use Clinton&apos;s AI
       </button>
